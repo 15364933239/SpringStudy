1d93e1a8ed0dfe1e294aec1e407716276afe8481
PeiTong Li <45646492+15364933239@users.noreply.github.com>
2019-03-22 11:52:21 +0800

    Create AccountService.java

---
+++ ssm_spring_mybatis_tx/src/service/AccountService.java
@@ -0,0 +1,16 @@
+package com.lpt.service;
+
+/**
+ * 
+ * @author lipeitong
+ * 2019年3月22日 上午10:54:01
+ * <dl>
+ * 		<dd>AccountService</dd>
+ * 		<dt>账户接口</dt>
+ * </dl>
+ */
+public interface AccountService {
+
+	//转账方法
+	void tranferAccount();
+}
